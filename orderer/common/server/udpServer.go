fac3f7207ec4429dd3d5140c9f3c619e28bcaab7
LinXJ1204 <39145342+LinXJ1204@users.noreply.github.com>
2024-12-18 13:10:07 +0800

    feat: little end

--- orderer/common/server/udpServer.go
+++ orderer/common/server/udpServer.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
 	"net"
@@ -61,14 +62,13 @@ func (us *UdpServer) Start() error {
 			}
 			extraBytes := buffer[n-4 : n] // The last 2 bytes are the extra bytes
 			fmt.Printf("Received extra bytes: %x\n", extraBytes)
+			var littleEndianValue uint64
 			// Create a larger byte slice to hold the uint64 value (8 bytes)
-			var paddedBytes [8]byte
-			// Copy the two bytes into the last two positions of the paddedBytes slice
-			copy(paddedBytes[4:], extraBytes)
-
-			// Convert to uint64 using BigEndian (for example)
-			extraUint64 := binary.LittleEndian.Uint64(paddedBytes[:])
-			fmt.Printf("Received extra bytes as uint64: %d\n", extraUint64)
+			err = binary.Read(bytes.NewReader(extraBytes), binary.LittleEndian, &littleEndianValue)
+			if err != nil {
+				fmt.Println("Error decoding Little Endian value:", err)
+			}
+			fmt.Printf("Little Endian interpreted value (uint64): %d (0x%x)\n", littleEndianValue, littleEndianValue)
 
 			// Unmarshal the remaining part into the Envelope struct (excluding the last 2 bytes)
 			envelope := &common.Envelope{}
@@ -97,7 +97,7 @@ func (us *UdpServer) Start() error {
 					continue
 				}
 
-				err = processor.Order(envelope, configSeq, 1, extraUint64)
+				err = processor.Order(envelope, configSeq, 1, littleEndianValue)
 				if err != nil {
 					logger.Warningf("[channel: %s] Rejecting broadcast of normal message from %s with SERVICE_UNAVAILABLE: rejected by Order: %s", chdr.ChannelId, addr, err)
 					continue
